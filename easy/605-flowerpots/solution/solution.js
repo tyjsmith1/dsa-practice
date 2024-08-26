fb94f2456def95316ea844cce7f75f3d7481e61c
tylersmith <tyjsmith1@gmail.com>
2024-08-26 18:34:55 -0500

    leetcode 605 solved

--- easy/605-flowerpots/solution/solution.js
+++ easy/605-flowerpots/solution/solution.js
@@ -1,3 +1,15 @@
-export default function solution(input) {
-    return true
+export default function solution(sample, n) {
+    let i = 0
+    while (i < sample.length && n !== 0) {
+        let left = sample[i - 1]
+        let right = sample[i + 1]
+        let target = sample[i]
+
+        if (target === 0 && left !== 1 && right !== 1) {
+            i++
+            n--
+        }
+        i++
+    }
+    return n === 0
 }
